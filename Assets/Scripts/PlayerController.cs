509f84b5871970653c16296145acc693637a68ed
Austin Beeler <abeeler1@uncc.edu>
2016-05-27 13:16:55 -0400

    Simple movement and coliseum

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float MoveSpeed = 1;
+    public float MaxSpeed = 1.5f;
+
+    new Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        Vector2 moveDirection = new Vector2();
+        if (Input.GetKey(KeyCode.W))
+            moveDirection.y += 1;
+        if (Input.GetKey(KeyCode.S))
+            moveDirection.y -= 1;
+        if (Input.GetKey(KeyCode.D))
+            moveDirection.x += 1;
+        if (Input.GetKey(KeyCode.A))
+            moveDirection.x -= 1;
+
+        /* Slow movement by drag increase when not moving */
+        rigidbody.drag = moveDirection == Vector2.zero ? 8 : 3;
+
+        moveDirection.Normalize();
+        moveDirection *= MoveSpeed;
+
+        rigidbody.AddForce(moveDirection);
+        if (rigidbody.velocity.magnitude > MaxSpeed)
+            rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed;
+    }
+}
