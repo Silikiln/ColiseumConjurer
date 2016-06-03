ebe9022b2b137a45a2982a439245af47e6475884
Austin Beeler <abeeler1@uncc.edu>
2016-06-02 21:12:02 -0400

    Firing projectiles

---
+++ Assets/Fireball.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Hit");
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,33 @@ public class PlayerController : MonoBehaviour {
     public float MoveSpeed = 1;
     public float MaxSpeed = 1.5f;
 
+    public GameObject fireballPrefab;
+    public float projectileSpeed = 500;
+
     new Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
 	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            GameObject fireball = (GameObject)Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+
+            Vector2 angle = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            fireball.GetComponent<Rigidbody2D>().AddForce(angle.normalized * projectileSpeed);
+        }
+    }
 	
 	void FixedUpdate () {
+        Movement();
+    }
+
+    void Movement()
+    {
         Vector2 moveDirection = new Vector2();
         if (Input.GetKey(KeyCode.W))
             moveDirection.y += 1;
