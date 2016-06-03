09e6b1b511eceeab42778e489023b6f513b4456d
Austin Beeler <abeeler1@uncc.edu>
2016-06-03 18:11:12 -0400

    Enemies added, they chase and push you away

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+    public float knockbackPower = 5;
+    public float MoveSpeed = .75f;
+    public float MaxSpeed = 1;
+
+    public Transform target;
+
+    new Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = target.position - transform.position;
+        rigidbody.AddForce(moveDirection.normalized * MoveSpeed);
+        if (rigidbody.velocity.magnitude > MaxSpeed)
+            rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer != 8) return;
+
+        GameObject player = coll.gameObject;
+        Rigidbody2D otherRigid = player.GetComponent<Rigidbody2D>();
+        otherRigid.velocity = Vector2.zero;
+        otherRigid.AddForce(((Vector2)(player.transform.position - transform.position)).normalized * knockbackPower);
+
+    }
+}
--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Fireball : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log("Hit");
         Destroy(gameObject);
     }
 }
