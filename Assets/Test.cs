3275a55f4aa2648c2518fae8d0ad2cc13d98effc
Austin Beeler <abeeler1@uncc.edu>
2016-06-15 12:43:07 -0400

    Spent far too much time working on an XMLParse attribute for future use

---
+++ Assets/Test.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+[XmlParse("Test")]
+public class Test {
+    static Test() {
+        Debug.Log("Loading test files");
+        List<Test> loadTest = XmlParseAttribute.ReadFileIntoList<Test>("Assets/Test.xml");
+        foreach (Test t in loadTest)
+            Debug.Log(t.ToString());
+    }
+
+    public enum TestEnum { Have, HaveNot, Neither }
+
+    [XmlParse("SubTest")]
+    public class SubTest
+    {
+        [XmlParse("Reason")]
+        public string Reason { get; private set; }
+
+        public override string ToString()
+        {
+            return "SubTest: " + Reason;
+        }
+    }
+
+    [XmlParse("Name")]
+    public string Name { get; private set; }
+
+    [XmlParse("Description")]
+    public string Description { get; private set; }
+
+    [XmlParse("Value")]
+    public int Value { get; private set; }
+
+    [XmlParse("Enum")]
+    public TestEnum Enum { get; private set; }
+
+    [XmlParse("SubTests")]
+    public List<SubTest> SubTests
+    {
+        get; private set;
+    }
+
+    [XmlParse("Items")]
+    public List<int> Items { get; private set; }
+
+    public override string ToString()
+    {
+        string subtests = "Subtests: " + SubTests.Count;
+        foreach (SubTest sub in SubTests)
+            subtests += "\n   " + sub.ToString();
+
+        string items = "Items: " + Items.Count;
+        foreach (int item in Items)
+            items += "\n   " + item;
+
+        return string.Format("{0}\n{1}\n{2}\n{3}\n{4}\n{5}", Name, Description, Value, Enum, subtests, items);
+    }
+}
---
+++ Assets/TestBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(typeof(Test).TypeHandle);
+    }
+}
