8fec885efa06f987e6a5f8c7b6962de297844389
Patrick LaChance <HurricusGaming@gmail.com>
2016-06-19 01:54:58 -0400

    Creation of the Chase trial.

---
+++ Assets/Scripts/Trials/Chase/Chase.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+using Random = UnityEngine.Random;
+
+public class Chase : Trial
+{
+    static float MinimumDistance = 1;
+    Vector2 lastPosition;
+    Vector2 chasePosition;
+    Vector2 startPosition;
+    float targetRadius = 1.5f;
+    float movementDuration;
+    float movement = 0;
+    float startTime;
+    float distance;
+
+    public Chase()
+    {
+        Name = "Chase";
+        Description = "Follow The Leader";
+
+        BaseObjectiveCount = 1;
+        chasePrefab = LoadResource<GameObject>("ChasePoint");
+        detectorPrefab = LoadResource<GameObject>("ChaseDetector");
+    }
+    
+    GameObject chasePrefab;
+    GameObject detector;
+    GameObject detectorPrefab;
+    SpriteRenderer detectorRenderer;
+    Boolean begin = false;
+    Boolean moving = false;
+    ChasePointController chasePoint;
+
+    public override void Setup()
+    {
+        do
+        {
+            chasePosition = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+        } while (Vector3.Distance(lastPosition, chasePosition) <= MinimumDistance);
+        lastPosition = chasePosition;
+
+        chasePoint = Instantiate(chasePrefab, chasePosition, Quaternion.identity).GetComponent<ChasePointController>();
+        detector = Instantiate(detectorPrefab, chasePoint.gameObject.transform.position, Quaternion.identity);
+        detector.transform.parent = chasePoint.gameObject.transform;
+
+        detectorRenderer = detector.GetComponent<SpriteRenderer>();
+        float currentRadius = detectorRenderer.sprite.bounds.extents.x;
+        float newScaleScalar = targetRadius / currentRadius;
+        Vector3 newScale = Vector3.one * newScaleScalar;
+        newScale.z = 1;
+        detector.transform.localScale = newScale;
+
+        begin = true;
+    }
+
+    void Update()
+    {
+        if (RequirementsMet)
+            TrialHandler.Instance.EventFinished();
+
+        if (!begin)
+            return;
+
+        distance = Vector2.Distance(PlayerController.Instance.transform.position, chasePoint.transform.position);
+        if (distance < 1.5)
+        {
+            chasePoint.BeingFilled = true;
+        }
+        else
+        {
+            chasePoint.BeingFilled = false;
+        }
+
+        if (moving)
+        {
+            movement += (Time.deltaTime) / movementDuration;
+            chasePoint.transform.position = Vector3.Lerp(startPosition, chasePosition, movement);
+        }
+        else
+        {
+            chasePosition = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+            startPosition = chasePoint.transform.position;
+            movementDuration = 2;
+            //print(movement);
+            movement = 0;
+            moving = true;
+            
+        }
+
+        if (movement >= 1)
+        {
+            moving = false;
+        }
+    }
+    public override bool RequirementsMet
+    {
+        get { return chasePoint != null ? chasePoint.Full : false; }
+    }
+}
---
+++ Assets/Scripts/Trials/Chase/ChasePointController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChasePointController : MonoBehaviour
+{
+    static float LightFillDuration = 10;
+
+    public Color FillColor;
+
+    public bool BeingFilled { get; set; }
+    public bool Full { get { return lightLevel >= 1; } }
+
+    Color startColor;
+    float lightLevel = 0;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!BeingFilled) return;
+
+        lightLevel += Time.deltaTime / LightFillDuration;
+        spriteRenderer.color = Color.Lerp(startColor, FillColor, lightLevel);
+    }
+}
