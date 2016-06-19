f2e74d6c0f464d353d40ee82a18ff7c8b194f8f5
Austin Beeler <abeeler1@uncc.edu>
2016-06-18 23:33:10 -0400

    Updated HideAndSeek trial

--- Assets/Scripts/Trials/HideAndSeek/HideAndSeek.cs
+++ Assets/Scripts/Trials/HideAndSeek/HideAndSeek.cs
@@ -6,75 +6,93 @@ using Random = UnityEngine.Random;
 
 public class HideAndSeek : Trial
 {
-    static float MinimumDistance = 1;
-    Vector2 lastPosition;
-    Vector2 endPosition;
+    static float MinimumDistance = .1f;
+    static float ExpandDuration = 1;
+    static float ExpandDelta = .1f;
+    static float ExpandAddedScale = 1f;
+    
     public HideAndSeek()
     {
         Name = "Hide And Seek";
         Description = "Find The Exit";
 
-        BaseObjectiveCount = 1;
-        pointPrefab = LoadResource<GameObject>("Point");
+        BaseObjectiveCount = 3;
         detectorPrefab = LoadResource<GameObject>("Detector");
     }
 
     int objectivesComplete = 0;
-    GameObject pointPrefab;
     GameObject detectorPrefab;
     GameObject detector;
     SpriteRenderer detectorRenderer;
-    Boolean begin = false;
+    Vector2 targetPosition;
+    IEnumerator objectiveCompleteCoroutine;
+    bool loaded = false;
 
     public override void Setup()
     {
-        if (lastPosition == null)
-            lastPosition = PlayerController.Instance.transform.position;
-        do
-        {
-            endPosition = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
-        } while (Vector3.Distance(lastPosition, endPosition) <= MinimumDistance);
-        lastPosition = endPosition;
-        
+        targetPosition = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+
         detector = Instantiate(detectorPrefab, PlayerController.Instance.transform.position, Quaternion.identity);
         detector.transform.parent = PlayerController.Instance.transform;
 
         detectorRenderer = detector.GetComponent<SpriteRenderer>();
-        begin = true;
+        loaded = true;
     }
 
     void Update()
     {
-        if(!begin)
-        {
-            return;
-        }
+        if (!loaded) return;
 
-        float distance = Vector2.Distance(PlayerController.Instance.transform.position, endPosition);
-        if (distance <= 0.1)
+        float distance = Vector2.Distance(PlayerController.Instance.transform.position, targetPosition);
+        if (distance <= MinimumDistance)
         {
             objectivesComplete++;
-            endPosition = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
             if (RequirementsMet)
                 TrialHandler.Instance.EventFinished();
+            else
+            {
+                targetPosition = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+                if (objectiveCompleteCoroutine != null)
+                    StopCoroutine(objectiveCompleteCoroutine);
+                objectiveCompleteCoroutine = ObjectiveCompleteNotice();
+                StartCoroutine(objectiveCompleteCoroutine);
+            }
         }
-        else if (distance < 1)
-        {
 
-            detectorRenderer.color = Color.green;
-        }
+        if (distance < 1.5)
+            detectorRenderer.color = Color.Lerp(Color.green, Color.yellow, distance / 1.5f);
         else if (distance < 3)
-        {
-            detectorRenderer.color = Color.yellow;
-        }
-        else
-        {
+            detectorRenderer.color = Color.Lerp(Color.yellow, Color.red, (distance - 1.5f) / 1.5f);
+        else        
             detectorRenderer.color = Color.red;
-        }
     }
 
     public override bool RequirementsMet
     {
         get { return objectivesComplete >= ObjectiveCount; }
+    }
+
+    IEnumerator ObjectiveCompleteNotice()
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < ExpandDuration)
+        {
+            yield return new WaitForSeconds(ExpandDelta);
+            timeElapsed += ExpandDelta;
+            Vector3 scale = Vector3.one * (1 + ExpandAddedScale * timeElapsed / ExpandDuration);
+            scale.z = 1;
+            detector.transform.localScale = scale;
+        }
+
+        timeElapsed = 0;
+        while (timeElapsed < ExpandDuration)
+        {
+            yield return new WaitForSeconds(ExpandDelta);
+            timeElapsed += ExpandDelta;
+            Vector3 scale = Vector3.one * (1 + ExpandAddedScale - ExpandAddedScale * timeElapsed / ExpandDuration);
+            scale.z = 1;
+            detector.transform.localScale = scale;
+        }
     }
 }
